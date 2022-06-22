e00b7f3a3122a23757ab7353a87cc10916e10f8c
Ronroeandassociates <3321@holbertonschool.com>
2022-06-21 20:21:56 -0500

    Tasks 0 Minimum Operations
    In a text file, there is a single character H. Your text editor can execute only two operations in this file: Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
    
    Prototype: def minOperations(n)
    Returns an integer
    If n is impossible to achieve, return 0
    Example:
    
    n = 9
    
    H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
    
    Number of operations: 6

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,20 +3,38 @@
 """
 0 Minimum Operations
 
-In a text file, there is a single character H. Your text editor can execute only two operations in this file:
-Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed to
-result in exactly n H characters in the file
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file:
+Copy All and Paste. Given a number n, write a method that calculates
+the fewest number of operations needed to result in exactly n H characters
+in the file
 
 Prototype: def minOperations(n)
 Returns an integer
+
 If n is impossible to achieve, return 0
 """
 
+
 def minOperations(n):
-    """minOperations is a function that takes a number and returns the minimum number of operations needed to achieve it"""
-    if n == 0:
+    """minOperations is a function that takes a number and
+    returns the minimum number of operations needed to achieve it
+    Args:
+        n (int): the number of H characters to achieve
+
+    Returns:
+        int: the minimum number of operations needed to achieve n H characters
+    """
+    if n < 2 or type(n) is not int:
         return 0
-    elif n == 1:
-        return 1
-    else:
-        return minOperations(n-1) + minOperations(n-2) + 1
+    count = 1
+    lists = list()
+    value = n
+    while value != 1:
+        count += 1
+        if value % count == 0:
+            while value % count == 0 and value != 1:
+                value = value / count
+                lists.append(count)
+
+    return sum(lists)
