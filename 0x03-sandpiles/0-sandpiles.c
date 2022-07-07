dd072a648af7b25a00d0f60a3f81528c3bfbe5d7
Ronroeandassociates <3321@holbertonschool.com>
2022-07-06 19:47:35 -0500

    [MAKE Betty Happy]

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -3,45 +3,109 @@
 #include "sandpiles.h"
 
 /**
- * sandpiles_sum - sums two sandpiles and topples until sum is stable
- * @grid1: first sandpile
- * @grid2: second sandpile
- *
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: sandpiles stable
+ * @grid2: sandpiles stable
  */
-void sandpiles_sum(int grid1[3][3],
-									 int grid2[3][3])
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int col, row;
-	int flags[3][3] = {
-			{0, 0, 0},
-			{0, 0, 0},
-			{0, 0, 0}};
+	sum(grid1, grid2);
 
-	/* sum the two grids */
-	for (row = 0; row < 3; row++)
+	while (check(grid1) == 0)
 	{
-		for (col = 0; col < 3; col++)
+		print_grid(grid1);
+		changeCompare(grid1);
+	}
+}
+
+/**
+ * sum - computes the sum of two sandpiles
+ * @grid1: sandpiles stable
+ * @grid2: sandpiles stable
+ */
+void sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+}
+
+/**
+ * check - compare if the sandpiles is stable
+ * @grid1: sandpiles stable
+ *
+ * Return: 1 if stable or 0 not stable
+ */
+int check(int grid1[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
 		{
-			grid1[row][col] += grid2[row][col];
+			if (grid1[i][j] > 3)
+				return (0);
 		}
 	}
+	return (1);
+}
 
-	while (checkStablility(grid1, flags))
+/**
+ * changeCompare - compare each sandpiles and append
+ * the sum into another matrix
+ * @grid1: sandpiles stable
+ */
+void changeCompare(int grid1[3][3])
+{
+	int i, j, newGrid[3][3];
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			newGrid[i][j] = 0;
+
+	for (i = 0; i < 3; i++)
 	{
-		/* print before toppling */
-		printf("=\n");
-		print_grid(grid1);
-
-		/* every flag, topple cell */
-		for (row = 0; row < 3; row++)
+		for (j = 0; j < 3; j++)
 		{
-			for (col = 0; col < 3; col++)
+			if (grid1[i][j] > 3)
 			{
-				if (flags[row][col] == 1)
-				{
-					toppleCell(grid1, row, col);
-				}
+				grid1[i][j] = grid1[i][j] - 4;
+				if ((i - 1 >= 0) && (i - 1 < 3))
+					newGrid[i - 1][j] += 1;
+				if ((j - 1 >= 0) && (j - 1 < 3))
+					newGrid[i][j - 1] += 1;
+				if ((i + 1 >= 0) && (i + 1 < 3))
+					newGrid[i + 1][j] += 1;
+				if ((j + 1 >= 0) && (j + 1 < 3))
+					newGrid[i][j + 1] += 1;
 			}
 		}
+	}
+	sum(grid1, newGrid);
+}
+
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			t
+			{
+				if (j)
+					printf(" ");
+				printf("%d", grid[i][j]);
+			}
+		printf("\n");
 	}
 }
--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -2,8 +2,9 @@
 #define SANDPILES_H
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-int checkStablility(int grid[3][3], int flags[3][3]);
-void toppleCell(int grid[3][3], int row, int col);
-/* static void print_grid(int grid[3][3]); */
+static void print_grid(int grid[3][3]);
+void sum(int grid1[3][3], int grid2[3][3]);
+void changeCompare(int grid1[3][3]);
+int check(int grid1[3][3]);
 
 #endif
