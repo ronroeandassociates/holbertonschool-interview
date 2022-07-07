7a99cb3dcf31eb4a8f27e5a1fd0929115fa3b996
Ronroeandassociates <3321@holbertonschool.com>
2022-07-06 19:35:09 -0500

    check .h

--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -4,6 +4,6 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int checkStablility(int grid[3][3], int flags[3][3]);
 void toppleCell(int grid[3][3], int row, int col);
-static void print_grid(int grid[3][3]);
+/* static void print_grid(int grid[3][3]); */
 
 #endif
