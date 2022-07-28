fe08470454a1447341d082c2109f414966d501d7
Ronroeandassociates <3321@holbertonschool.com>
2022-07-28 18:09:08 -0500

    🐳 chore(Burn it Down): Take it back to 1

--- 0x04-linked_list_palindrome/lists.h
+++ 0x04-linked_list_palindrome/lists.h
@@ -1,8 +1,5 @@
-#ifndef __LISTS__H
-#define __LISTS__H
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct listint_s - singly linked list
@@ -14,8 +11,8 @@
  */
 typedef struct listint_s
 {
-  int n;
-  struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,6 +20,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int is_palindrome_helper(listint_t **left, listint_t **right);
 
-#endif /* __LISTS__H */
+#endif /* LISTS_H */
