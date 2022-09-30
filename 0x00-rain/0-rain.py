406f9495463b2eff1bab1f1a7c8aa6942d0ce45a
Ronroeandassociates <3321@holbertonschool.com>
2022-09-29 20:48:47 -0500

    fix issue with walls

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
+
 """
-0_rain
+Given a list of non-negative integers representing the heights of walls
+with unit width 1 calculate how many square units of water will be retained
+after it rains.
+Prototype: def rain(walls)
+walls is a list of non-negative integers.
+Return: Integer indicating total amount of rainwater retained.
+If the list is empty return 0.
 """
+
 
 def rain(walls):
-    if len(walls) < 1:
+    """Calculate units of water will be retained"""
+    if not walls:
         return 0
-
-    # Find the highest wall
-    amount = 0
-
-
-for key, value in enumerate(walls):
-    if key in [0, len(walls) - 1]:
-        continue
-    left = max(walls[:key])
-    right = max(walls[key + 1:])
-    if value < left and value < right:
-        amount += min(left, right) - value
+    water = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
+    return water
