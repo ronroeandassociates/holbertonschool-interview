fc90515ff831be02e19b6a6b46ef7b039700f1fb
Ronroeandassociates <3321@holbertonschool.com>
2022-11-21 14:39:22 -0600

    pycodestyled

--- 0x15-island_perimeter/0-island_perimeter.py
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 '''
-function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+function def island_perimeter(grid):
+that returns the perimeter of the island described in grid:
 grid is a list of list of integers:'''
+
 
 def island_perimeter(grid):
     land = 0
