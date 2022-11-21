cc591faa852ffc279853414276e3c5ff4fe76c37
Ronroeandassociates <3321@holbertonschool.com>
2022-11-21 13:21:12 -0600

    set up a grid

---
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+grid is a list of list of integers:'''
+
+def island_perimeter(grid):
+    land = 0
+    connect = 0
+    for down in range(len(grid)):
+        for across in range(len(grid[down])):
+            if grid[down][across] == 1:
+                land += 1
+                if down > 0 and grid[down - 1][across] == 1:
+                    connect += 1
+                if across > 0 and grid[down][across - 1] == 1:
+                    connect += 1
+    return land * 4 - connect * 2
